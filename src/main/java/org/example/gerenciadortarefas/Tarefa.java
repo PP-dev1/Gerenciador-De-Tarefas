e195ce1b259ec20a5612a43aa0d78cc332e499c4
kauaguedes633-cloud <kauaguedes633@gmail.com>
2026-06-07 16:56:04 -0300

    Adiciona marcação de tarefa concluída

--- src/main/java/org/example/gerenciadortarefas/Tarefa.java
+++ src/main/java/org/example/gerenciadortarefas/Tarefa.java
@@ -5,11 +5,13 @@ public class Tarefa {
     private String nome;
     private String descricao;
     private String prioridade;
+    private String status;
 
     public Tarefa(String nome, String descricao, String prioridade) {
         this.nome = nome;
         this.descricao = descricao;
         this.prioridade = prioridade;
+        this.status = "Pendente";
     }
 
     public String getNome() {
@@ -22,5 +24,13 @@ public class Tarefa {
 
     public String getPrioridade() {
         return prioridade;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
